9ad8c3a5833ef0934802d09767aa61ae9c9307a4
mary <adams222@uwm.edu>
2020-05-26 18:21:16 -0500

    editing classes

--- childPiece/knight.py
+++ childPiece/knight.py
@@ -2,7 +2,11 @@ from parentPiece.piece import Piece
 
 
 class Knight(Piece):
-    def __init__(self, pMove, pPoints, pColor):
-        self.move = pMove
-        self.points = pPoints
-        self.color = pColor
+    def __init__(self, piece_move_cnt, piece_points, piece_color, piece_curr_loc):
+        self.move_count = piece_move_cnt
+        self.points = piece_points
+        self.color = piece_color
+        self.curr_loc = piece_curr_loc
+
+    def move_loc(self):
+        pass
--- parentPiece/piece.py
+++ parentPiece/piece.py
@@ -1,6 +1,24 @@
-class Piece:
-    def __init__(self, pMove, pRank, pColor, pActive):
-        self.move = pMove
-        self.rank = pRank
-        self.color = pColor
-        self.active = pActive
+from abc import ABC, abstractmethod
+
+
+class Piece(ABC):
+    def __init__(self, piece_move_cnt, piece_points, piece_color, piece_crnt_loc):
+        self.move_count = piece_move_cnt
+        self.points = piece_points
+        self.color = piece_color
+        self.curr_loc = piece_crnt_loc
+
+    def __del__(self):
+        pass
+
+    @abstractmethod
+    def move_loc(self):
+        # player can only move when it his/her turn first checks if king is in check:
+        # each piece has its own movement rules cannot move through its own piece(except for the knight)
+        pass
+
+    def move(self):
+        pass
+
+    def capture(self):
+        pass
