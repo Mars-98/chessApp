e3c426fdb701ea3527448c857155a53f6e2b3b4e
mary <adams222@uwm.edu>
2020-05-29 23:24:31 -0500

    #76jre6

--- chess-api/parentPiece/parentPiece.py
+++ chess-api/parentPiece/parentPiece.py
@@ -4,11 +4,12 @@ from abc import ABC, abstractmethod
 class Piece(ABC):
     totalPieces = 16
 
-    def __init__(self, piece_move_cnt, piece_points, piece_color, piece_curr_loc):
+    def __init__(self, piece_move_cnt, piece_points, piece_color, piece_curr_loc, piece_first_move):
         self.move_count = piece_move_cnt
         self.points = piece_points
         self.color = piece_color
         self.curr_loc = piece_curr_loc
+        self.first_move = piece_first_move
 
     def __del__(self, piece_move_cnt, piece_points, piece_color, piece_curr_loc):
         del piece_move_cnt
@@ -21,9 +22,6 @@ class Piece(ABC):
         # player can only move when it his/her turn first checks if king is in check:
         # each piece has its own movement rules cannot move through its own piece(except for the knight)
         pass
-
-    def move(self, new_loc, piece):
-        piece.curr_loc = new_loc
 
     def capture(self, my_piece, enemy_piece):
         if not my_piece.color == enemy_piece.color:
