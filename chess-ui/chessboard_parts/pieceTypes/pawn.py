d2d10b604330d7522d5c68bdc245671927cf1297
coughenberg <khfantk@gmail.com>
2020-05-30 17:01:31 -0500

    #78gumw Fixed up placement

--- chess-ui/chessboard_parts/pieceTypes/pawn.py
+++ chess-ui/chessboard_parts/pieceTypes/pawn.py
@@ -3,20 +3,21 @@ from pygame.sprite import Sprite
 import os
 
 class Pawn(Sprite):
-    placement = (0,0)
+    placement = (0, 0)
     color_black = True
     piece_size = None
 
     def __init__(self, window, placement, color_black):
         pygame.sprite.Sprite.__init__(self)
-        self.placement = placement
         self.color_black = color_black
-        self.piece_size = pygame.display.get_surface().get_height()/14 # This division probably works or equivalent integer @Connor
+        self.piece_size = pygame.display.get_surface().get_height()/14
         if color_black:
-            script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+            self.placement = (1, placement) 
+            script_dir = os.path.dirname(__file__)
             rel_path = "../bit_pieces/chess_piece_2_black_pawn.bmp"
             os.path.join(script_dir, rel_path)
         else:
-            script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+            self.placement = (6, placement)
+            script_dir = os.path.dirname(__file__)
             rel_path = "../bit_pieces/chess_piece_2_white_pawn.bmp"
             os.path.join(script_dir, rel_path)
--- chess-ui/chessboard_parts/pieceTypes/rook.py
+++ chess-ui/chessboard_parts/pieceTypes/rook.py
@@ -3,20 +3,21 @@ from pygame.sprite import Sprite
 import os
 
 class Rook(Sprite):
-    placement = (0,0)
+    placement = (0, 0)
     color_black = True
     piece_size = None
 
     def __init__(self, window, placement, color_black):
         pygame.sprite.Sprite.__init__(self)
-        self.placement = placement
         self.color_black = color_black
         self.piece_size = pygame.display.get_surface().get_height()/14
         if color_black:
-            script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+            self.placement = (0, placement) 
+            script_dir = os.path.dirname(__file__)
             rel_path = "../bit_pieces/chess_piece_2_black_rook.bmp"
             os.path.join(script_dir, rel_path)
         else:
-            script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+            self.placement = (7, placement) 
+            script_dir = os.path.dirname(__file__)
             rel_path = "../bit_pieces/chess_piece_2_white_rook.bmp"
             os.path.join(script_dir, rel_path)
